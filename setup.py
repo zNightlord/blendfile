23ef11c9dcf4e83cbaa123d244eb8922c2937c69
Dalai Felinto <dfelinto@gmail.com>
2016-12-01 16:45:05 +0100

    Leave setup.py version as 1.0, to push to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='blender_file',
-    version='0.1.1',
+    version='1.0',
     description='Blender File',
     url='https://developer.blender.org/diffusion/BBF/',
     author='At Mind B.V. - Jeroen Bakker, Blender Foundation - Campbell Barton',
