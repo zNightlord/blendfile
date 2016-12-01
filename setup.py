0ceede74c162697fd1a52ac37f0b500ba12440e8
Dalai Felinto <dfelinto@gmail.com>
2016-12-01 14:58:59 +0100

    Using the new official repository

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='blender_file',
     version='0.1.1',
     description='Blender File',
-    url='https://github.com/dfelinto/blender-file/',
+    url='https://developer.blender.org/diffusion/BBF/',
     author='At Mind B.V. - Jeroen Bakker, Blender Foundation - Campbell Barton',
     author_email='foundation@blender.org',
     license='GNU General Public License v2 or later (GPLv2+)',
